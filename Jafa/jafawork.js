216eef57bf612f0fcfee407ae0074bdd1098b756
ShathaAthamneh <sathamneh5@gmail.com>
2024-07-16 20:07:16 +0300

    jj

--- Jafa/jafawork.js
+++
@@ -1,7 +0,0 @@
-<script>
-// Example JavaScript code
-document.addEventListener('DOMContentLoaded', function() {
-    // Your JavaScript code goes here
-    console.log('Document loaded')
-});
-</script>
--- Jafa/task01.js
+++
@@ -1,2 +0,0 @@
-const userName = prompt("Please enter your name:");
-alert(`Welcome, ${userName}!`);
