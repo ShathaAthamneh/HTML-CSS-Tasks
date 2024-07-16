adbdd630f1ce8e9b214e78e20503608589d4cc7e
ShathaAthamneh <sathamneh5@gmail.com>
2024-07-16 15:00:55 +0300

    jafaworka

---
+++ Jafa/jafawork.js
@@ -0,0 +1,7 @@
+<script>
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code goes here
+    console.log('Document loaded')
+});
+</script>
